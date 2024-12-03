6a70657d512bd4e0d7cc40d25eca3d99ac640b54
Hsu Shen (Angus) Lee <62867164+drunkint@users.noreply.github.com>
2024-12-03 22:16:43 +0800

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
+
+fn handle_not_found(input: &str) {
+    println!("{}: not found", input);
+}
+
+fn handle_exit(input: Vec<&str>) {
+    if input.len() != 2 || input[0] != "exit" || input[1].parse::<i32>().is_err() {
+        handle_not_found(input[0]);
+        return;
+    }
+
+    exit(input[1].parse::<i32>().ok().unwrap());
+}
 
 fn main() {
-    // Uncomment this block to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -12,10 +25,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         
-        let input = input.trim();
+        let input: Vec<&str> = input.trim().split(' ').collect();
+
+        if input.len() <= 0 {
+            continue;
+        }
     
-        match input {
-            _ => println!("{}: not found", input),
+        match input[0] {
+            "exit" => handle_exit(input),
+            _ => handle_not_found(input[0]),
         }
     }
 
